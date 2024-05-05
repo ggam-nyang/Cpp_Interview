0735dd1b3f235c61cb5b3019a60f0612be817aa2
ggam-nyang <v44ads@naver.com>
2024-05-05 23:43:57 +0900

    코딩인터뷰 5-4

---
+++ Chapter5/5-4/5-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main() {
+
+
+    for (const auto &c: getMultiple1(A, B)) {
+        cout << c << endl;
+    }
+}
