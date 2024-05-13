39780f763fdea47ee423f74471156bfde5c71870
ggam-nyang <v44ads@naver.com>
2024-05-13 13:19:40 +0900

    코딩인터뷰 5-7

--- Chapter5/5-7/5-7.cpp
+++ Chapter5/5-7/5-7.cpp
@@ -29,6 +29,26 @@ double BuyAndSellStockTwice(const vector<double> &prices) {
     return max_profit;
 }
 
+double BuyAndSellStockTwice2(const vector<double> &prices) {
+    double max_total_profit = 0;
+    vector<double> first_buy_sell_profits(prices.size(), 0);
+    double min_price_so_far = std::numeric_limits<double>::infinity();
+
+    for (int i = 0; i < size(prices); ++i) {
+        min_price_so_far = std::min(min_price_so_far, prices[i]);
+        max_total_profit = std::max(max_total_profit, prices[i] - min_price_so_far);
+        first_buy_sell_profits[i] = max_total_profit;
+    }
+
+    double max_price_so_far = std::numeric_limits<double>::min();
+    for (int i = size(prices) - 1; i > 0; --i) {
+        max_price_so_far = std::max(max_price_so_far, prices[i]);
+        max_total_profit = std::max(max_total_profit, first_buy_sell_profits[i] + max_total_profit - prices[i]);
+    }
+
+    return max_total_profit;
+}
+
 int main() {
     vector<double> A;
     int N;
