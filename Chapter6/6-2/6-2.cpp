b0d2949f126fa1519f4159495c522a175a7ba6fe
ggam-nyang <v44ads@naver.com>
2024-05-17 00:20:16 +0900

    코딩인터뷰 6-2

--- Chapter6/6-2/6-2.cpp
+++ Chapter6/6-2/6-2.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int ConvertTen(const string& num_as_string, int b1) {
-    return accumulate(num_as_string.begin(), num_as_string.end(), 0,
-                      [b1](int sum, char c){ return sum * b1 + c >= 'A' ? c - 'A' + 10 : c - '0'; });
+    return accumulate(num_as_string.begin() + (num_as_string[0] == '-' || num_as_string[0] == '+'), num_as_string.end(), 0,
+                      [b1](int sum, char c){ return sum * b1 + (c >= 'A' ? c - 'A' + 10 : c - '0'); });
 }
 
 string ConvertBase(const string& num_as_string, int b1, int b2) {
@@ -14,10 +14,12 @@ string ConvertBase(const string& num_as_string, int b1, int b2) {
     string ans;
     do {
         int temp = decimalNum % b2;
+        ans.push_back(temp < 10 ? '0' + temp : 'A' + temp - 10);
+        decimalNum /= b2;
+    } while (decimalNum);
+    if (num_as_string[0] == '-') ans.push_back('-');
 
-    } while (decimalNum)
-
-    return "";
+    return {ans.rbegin(), ans.rend()};
 }
 
 int main() {
--- Chapter6/6-2/README.md
+++ Chapter6/6-2/README.md
@@ -6,4 +6,16 @@
 바로 떠오르는 방법은, 10진수로 변환하여 다시 변환하는 방법이다..!  
 문자열의 길이 제한이 없다고 했을 때, double로 변환하고, 다시 n진수로 변환한다.
 
+풀고보니, 6-1과 완벽하게 동일하다..!  6-1은 IntToString이지만 사실 string -> 10진수이고,   
+이번 문제는 확장된 n진수 -> string, string -> n진수이다!
 
+시간은 1us 미만으로 성공이다 ㅎㅎ
+
+### 해법
+동일한 풀이이다. 다만, 10진수를 변환할 때 재귀를 이용했다.  반복문은 늘 재귀로 바꿀 수 있으니 당연하지만 알아두면 좋을 것 같다.  
+
+```c++
+std::string ConstructFromBase(int num_as_int, int base) {
+    return num_as_int == 0 ? "" : ConstructFromBase(num_as_int / base, base) + static_cast<char>(num_as_int % base >= 10 ? 'A' + num_as_int % base - 10 : '0' + num_as_int % base);
+}
+```
